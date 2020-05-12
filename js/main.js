d9af0e2cfc309e8a0657003b05b3c3b8d9c8cf87
Yeasin123 <yeasin15-1037@diu.edu.bd>
2020-05-12 14:38:27 +0600

    Unique Web Design

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+ jQuery(function($) {
+      $(document).ready( function() {
+        //enabling stickUp on the '.navbar-wrapper' class
+        $('.navbar-wrapper').stickUp();
+     });
+ });
+
+//WOW.js code
+
+ new WOW().init();
+
+
+//Initialize material-scrolltop (minimal)
+
+  $('body').materialScrollTop();
